c9a9239f061fea494ebdc39b182e7bf01b0bf80a
Henrico Vilela <henricoVilela@gmail.com>
2025-04-07 17:36:10 -0400

    Add dto and account_service

---
+++ gateway-api/internal/dto/acconunt.go
@@ -0,0 +1,36 @@
+package dto
+
+import "github.com/henricoVilela/gateway-pagamento/gateway-api/internal/domain"
+
+type CreateAccountInput struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type AccountOutput struct {
+	ID        string  `json:"id"`
+	Name      string  `json:"name"`
+	Email     string  `json:"email"`
+	Balance   float64 `json:"balance"`
+	CreatedAt string  `json:"created_at"`
+	UpdatedAt string  `json:"updated_at"`
+	APIKey    string  `json:"api_key,omitempty"`
+}
+
+func ToAccount(input CreateAccountInput) *domain.Account {
+	return &domain.Account{
+		Name:  input.Name,
+		Email: input.Email,
+	}
+}
+
+func FromAccount(account *domain.Account) *AccountOutput {
+	return &AccountOutput{
+		ID:        account.ID,
+		Name:      account.Name,
+		Email:     account.Email,
+		Balance:   account.Balance,
+		CreatedAt: account.CreatedAt.Format("2006-01-02 15:04:05"),
+		UpdatedAt: account.UpdatedAt.Format("2006-01-02 15:04:05"),
+	}
+}
--- gateway-api/internal/service/account_service.go
+++ gateway-api/internal/service/account_service.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/henricoVilela/gateway-pagamento/gateway-api/internal/domain"
+import (
+	"github.com/henricoVilela/gateway-pagamento/gateway-api/internal/domain"
+	"github.com/henricoVilela/gateway-pagamento/gateway-api/internal/dto"
+)
 
 type AccountService struct {
 	repository domain.AccountRepository
@@ -8,4 +11,38 @@ type AccountService struct {
 
 func NewAccountService(repository domain.AccountRepository) *AccountService {
 	return &AccountService{repository: repository}
+}
+
+func (s *AccountService) Create(input *dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(*input)
+
+	existingAccount, err := s.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicatedApiKey
+	}
+
+	err = s.repository.Save(account)
+	if err != nil {
+		return nil, err
+	}
+
+	return dto.FromAccount(account), nil
+}
+
+func (s *AccountService) UpdateBalance(apiKey string, amount float64) (*dto.AccountOutput, error) {
+	account, err := s.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	account.AddBalance(amount)
+	err = s.repository.UpdateBalance(account)
+	if err != nil {
+		return nil, err
+	}
+	return dto.FromAccount(account), nil
 }
